92e42a4c33d9c10ff76c53d2ad65e575cb77d383
Diyar Kudrat <dkudrat@gmail.com>
2020-03-31 12:57:50 -0700

    completed number bases starter code

--- Code/all-starter-code/bases.py
+++ Code/all-starter-code/bases.py
@@ -17,13 +17,12 @@ def decode(digits, base):
     return: int -- integer representation of number (in base 10)"""
     # Handle up to base 36 [0-9a-z]
     assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
-    # TODO: Decode digits from binary (base 2)
-    # ...
-    # TODO: Decode digits from hexadecimal (base 16)
-    # ...
-    # TODO: Decode digits from any base (2 up to 36)
-    # ...
 
+    decoded = 0
+    for i, num in enumerate(reversed(digits)):
+        decoded += base**i * string.printable.index(num)
+
+    return decoded
 
 def encode(number, base):
     """Encode given number in base 10 to digits in given base.
@@ -34,13 +33,12 @@ def encode(number, base):
     assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
     # Handle unsigned numbers only for now
     assert number >= 0, 'number is negative: {}'.format(number)
-    # TODO: Encode number in binary (base 2)
-    # ...
-    # TODO: Encode number in hexadecimal (base 16)
-    # ...
-    # TODO: Encode number in any base (2 up to 36)
-    # ...
-
+    
+    encoded_num = ""
+    while number > 0:
+        number, remainder = divmod(number, base)
+        encoded_num += string.printable[remainder]
+    return encoded_num[::-1]
 
 def convert(digits, base1, base2):
     """Convert given digits in base1 to digits in base2.
@@ -59,6 +57,9 @@ def convert(digits, base1, base2):
     # ...
     # TODO: Convert digits from any base to any base (2 up to 36)
     # ...
+
+    return encode(decode(digits, base1), base2)
+    
 
 
 def main():
